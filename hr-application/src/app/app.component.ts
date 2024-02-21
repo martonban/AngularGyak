db79f12853950d04e4ff5b53aef27ca68c31b375
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-02-21 19:33:17 +0100

    Lab02 Almost DONE
    There is a problem

--- hr-application/src/app/app.component.ts
+++ hr-application/src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Applicant, applicants } from './applicants';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hr-application';
+  applicants: Applicant[] = applicants;
+
+  isFormVisible: boolean = false;
+
+  newApplicant : Applicant = this.defaultApplicant();
+
+  deleteApplicant(applicant : Applicant) {
+    const index = this.applicants.indexOf(applicant);
+    if(index > -1) {
+      this.applicants.splice(index, 1);
+    }
+  }
+
+  addApplicant() {
+    this.applicants.push(this.newApplicant)
+    this.newApplicant = this.defaultApplicant();
+    this.isFormVisible = false;
+  }
+
+  openForm() {
+    this.isFormVisible = true;
+  }
+
+  defaultApplicant(): Applicant {
+    return {
+      id: 0,
+      name: "",
+      role: "",
+      experience: "0-5",
+      desiredSalary: 400000
+    }
+  }
 }

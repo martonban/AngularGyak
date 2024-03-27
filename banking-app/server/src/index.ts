aa0e31e27bbccc2ba75d87090edff0d9a44f529a
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-27 18:51:35 +0100

    express created

--- banking-app/server/src/index.ts
+++ banking-app/server/src/index.ts
@@ -1,20 +1,26 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+// "esModuleInterop": true, a serveren belüli tsconfig-ba
+import express from 'express';
 
-AppDataSource.initialize().then(async () => {
+async function main() {
+  try {
+    // Vezérlésnek addig dokkolódni kell amíg nem tré vissza valamivel a függvényünk.
+    await AppDataSource.initialize();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    // Express server létrehozása
+    const app = express();
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    // TODO :: routers
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    // Server elindítása
+    app.listen(3000, () => {
+      console.log("Listening on port 3000...");
+    });
 
-}).catch(error => console.log(error))
+  }catch (err) {
+    console.error(err);
+  }
+}
+
+main();
+

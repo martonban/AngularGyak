f3047e8b7955972692f4d973c9059567284b5645
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-27 18:21:09 +0100

    init banking app

--- banking-app/server/src/data-source.ts
+++ banking-app/server/src/data-source.ts
@@ -6,11 +6,12 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "root", // in case of no password, set it to undefined, or delete this row
-    database: "infrend2024_typeorm",
+    username: "test",
+    password: "test",
+    database: "test",
     synchronize: true,
-    logging: true,
+    logging: false,
     entities: [User],
     migrations: [],
     subscribers: [],
-});
+})

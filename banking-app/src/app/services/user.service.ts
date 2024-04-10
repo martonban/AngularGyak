1faaa2989bd6cdba39da6f5595bcf1f3fe0da877
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-04-10 19:17:29 +0200

    Valami történik, de hogy mi azt nem tudom :/

--- banking-app/src/app/services/user.service.ts
+++ banking-app/src/app/services/user.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { UserDTO } from '../../../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  http = inject(HttpClient);
+
+  getAll() {
+    return this.http.get<UserDTO[]>('/api/user');    
+  }
+
+  getOne(id: number) {
+    return this.http.get<UserDTO>('/api/user/' + id);    
+  }
+
+  create(user: UserDTO) {
+    return this.http.post<UserDTO>('/api/user', user);
+  }
+
+  update(user: UserDTO) {
+    return this.http.put<UserDTO>('/api/user', user);
+  }
+
+  delete(id: number) {
+    return this.http.delete('/api/user/' + id); 
+  }
 }

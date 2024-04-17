b80e8cb52cd84197a2c9c79d4ac734a46bb9f40b
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-04-17 18:17:43 +0200

    Add User init

--- banking-app/src/app/app.routes.ts
+++ banking-app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: UserListComponent
+    },
+    {
+        path: 'add-user',
+        component: UserFormComponent
     }
 ];
